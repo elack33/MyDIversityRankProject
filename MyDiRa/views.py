f48f2151dceb2ec273caa01f4089089899c40667
Dar Wright <elack33@gmail.com>
2015-09-10 19:21:39 -0700

    Chart is filled with data from my database!

--- MyDiRa/views.py
+++ MyDiRa/views.py
@@ -39,7 +39,7 @@ def manual_entry(request):
             author = request.user
             # print 'Before save'
             form.save()
-            return HttpResponseRedirect(reverse('manual_entry'))
+            return HttpResponseRedirect(reverse('manual'))
 
 
     else:
@@ -57,19 +57,137 @@ def manual_entry(request):
 @login_required
 def show_data(request):
     author = request.user
-    total_surveys = SurveyResponses.objects.filter(author_id=author).count()
-    white_people = SurveyResponses.objects.filter(author_id=2, demographic__demographic='White').count()
+    # total_surveys = SurveyResponses.objects.filter(author_id=author).count()
+    # white_people = SurveyResponses.objects.filter(author_id=2, demographic__demographic='White').count()
 
     # get_my_data = get_data(request)
     # print get_my_data
 
-    accounting = SurveyResponses.objects.filter(author_id=author, career01__career='Accounting').count()
-    print accounting
+    # accounting = SurveyResponses.objects.filter(author_id=author, career01__career='Accounting').count()
+    # accounting = SurveyResponses.objects.filter(author_id=author, career01__career='Accounting').count()
+    # print "accounting", accounting
+
+    accounting = (SurveyResponses.objects.filter(author_id=author, career01__career='Accounting').count())
+    general_business = SurveyResponses.objects.filter(author_id=author, career01__career='General Business ').count()
+    software_development = SurveyResponses.objects.filter(author_id=author, career01__career='Software Development').count()
+    admin_clerical = SurveyResponses.objects.filter(author_id=author, career01__career='Admin & Clerical').count()
+    general_labor = SurveyResponses.objects.filter(author_id=author, career01__career='General Labor').count()
+    pharmaceutical = SurveyResponses.objects.filter(author_id=author, career01__career='Pharmaceutical').count()
+    automotive = SurveyResponses.objects.filter(author_id=author, career01__career='Automotive').count()
+    government = SurveyResponses.objects.filter(author_id=author, career01__career='Government').count()
+    professional_services = SurveyResponses.objects.filter(author_id=author, career01__career='Professional Services').count()
+    banking = SurveyResponses.objects.filter(author_id=author, career01__career='Banking').count()
+    grocery = SurveyResponses.objects.filter(author_id=author, career01__career='Grocery').count()
+    purchasing_procurement = SurveyResponses.objects.filter(author_id=author, career01__career='Purchasing-Procurement').count()
+    biotech = SurveyResponses.objects.filter(author_id=author, career01__career='Biotech').count()
+    health_care = SurveyResponses.objects.filter(author_id=author, career01__career='Health Care').count()
+    qa_quality_control = SurveyResponses.objects.filter(author_id=author, career01__career='QA-Quality Control').count()
+    journalism = SurveyResponses.objects.filter(author_id=author, career01__career='Journalism').count()
+    hotel_hospitality = SurveyResponses.objects.filter(author_id=author, career01__career='Hotel-Hospitality').count()
+    Real_Estate = SurveyResponses.objects.filter(author_id=author, career01__career='Real Estate').count()
+    business_development = SurveyResponses.objects.filter(author_id=author, career01__career='Business Development').count()
+    human_resources = SurveyResponses.objects.filter(author_id=author, career01__career='Human Resources').count()
+    research = SurveyResponses.objects.filter(author_id=author, career01__career='Research').count()
+    construction = SurveyResponses.objects.filter(author_id=author, career01__career='Construction').count()
+    information_technology = SurveyResponses.objects.filter(author_id=author, career01__career='Information Technology ').count()
+    restaurant_food_service = SurveyResponses.objects.filter(author_id=author, career01__career='Restaurant-Food Service').count()
+    consultant = SurveyResponses.objects.filter(author_id=author, career01__career='Consultant').count()
+    installation_repair = SurveyResponses.objects.filter(author_id=author, career01__career='Installation-Repair ').count()
+    retail = SurveyResponses.objects.filter(author_id=author, career01__career='Retail').count()
+    customer_service = SurveyResponses.objects.filter(author_id=author, career01__career='Customer Service').count()
+    insurance = SurveyResponses.objects.filter(author_id=author, career01__career='Insurance').count()
+    sales = SurveyResponses.objects.filter(author_id=author, career01__career='Sales').count()
+    design = SurveyResponses.objects.filter(author_id=author, career01__career='Design').count()
+    inventory = SurveyResponses.objects.filter(author_id=author, career01__career='Inventory').count()
+    science = SurveyResponses.objects.filter(author_id=author, career01__career='Science').count()
+    distribution_shipping = SurveyResponses.objects.filter(author_id=author, career01__career='Distribution-Shipping').count()
+    legal = SurveyResponses.objects.filter(author_id=author, career01__career='Legal').count()
+    skilled_labor_trades = SurveyResponses.objects.filter(author_id=author, career01__career='Skilled Labor-Trades').count()
+    education_teaching = SurveyResponses.objects.filter(author_id=author, career01__career='Education-Teaching').count()
+    legal_admin = SurveyResponses.objects.filter(author_id=author, career01__career='Legal Admin').count()
+    strategy_planning = SurveyResponses.objects.filter(author_id=author, career01__career='Strategy Planning').count()
+    engineering = SurveyResponses.objects.filter(author_id=author, career01__career='Engineering').count()
+    management = SurveyResponses.objects.filter(author_id=author, career01__career='Management').count()
+    supply_chain = SurveyResponses.objects.filter(author_id=author, career01__career='Supply Chain').count()
+    entry_level = SurveyResponses.objects.filter(author_id=author, career01__career='Entry Level ').count()
+    manufacturing = SurveyResponses.objects.filter(author_id=author, career01__career='Manufacturing').count()
+    telecommunications = SurveyResponses.objects.filter(author_id=author, career01__career='Telecommunications').count()
+    executive = SurveyResponses.objects.filter(author_id=author, career01__career='Executive').count()
+    marketing = SurveyResponses.objects.filter(author_id=author, career01__career='Marketing').count()
+    training = SurveyResponses.objects.filter(author_id=author, career01__career='Training').count()
+    facilities = SurveyResponses.objects.filter(author_id=author, career01__career='Facilities').count()
+    media = SurveyResponses.objects.filter(author_id=author, career01__career='Media').count()
+    newspaper = SurveyResponses.objects.filter(author_id=author, career01__career='Newspaper').count()
+    transportation = SurveyResponses.objects.filter(author_id=author, career01__career='Transportation').count()
+    finance = SurveyResponses.objects.filter(author_id=author, career01__career='Finance').count()
+    nonprofit = SurveyResponses.objects.filter(author_id=author, career01__career='Nonprofit').count()
+    social_services = SurveyResponses.objects.filter(author_id=author, career01__career='Social Services').count()
+    warehouse = SurveyResponses.objects.filter(author_id=author, career01__career='Warehouse').count()
+    other = SurveyResponses.objects.filter(author_id=author, career01__career='Other').count()
+
 
     return render(
         request,
         'show_data.html',
-        # context= get_my_data
+        context={
+            'accounting': accounting,
+            'general_business': general_business,
+            'software_development': software_development,
+            'admin_clerical': admin_clerical,
+            'general_labor': general_labor,
+            'pharmaceutical': pharmaceutical,
+            'automotive': automotive,
+            'government': government,
+            'professional_services': professional_services,
+            'banking': banking,
+            'grocery': grocery,
+            'purchasing_procurement': purchasing_procurement,
+            'biotech': biotech,
+            'health_care': health_care,
+            'qa_quality_control': qa_quality_control,
+            'journalism': journalism,
+            'hotel_hospitality': hotel_hospitality,
+            'Real_Estate': Real_Estate,
+            'business_development': business_development,
+            'human_resources': human_resources,
+            'research': research,
+            'construction': construction,
+            'information_technology': information_technology,
+            'restaurant_food_service': restaurant_food_service,
+            'consultant': consultant,
+            'installation_repair': installation_repair,
+            'retail': retail,
+            'customer_service': customer_service,
+            'insurance': insurance,
+            'sales': sales,
+            'design': design,
+            'inventory': inventory,
+            'science': science,
+            'distribution_shipping': distribution_shipping,
+            'legal': legal,
+            'skilled_labor_trades': skilled_labor_trades,
+            'education_teaching': education_teaching,
+            'legal_admin': legal_admin,
+            'strategy_planning': strategy_planning,
+            'engineering': engineering,
+            'management': management,
+            'supply_chain': supply_chain,
+            'entry_level': entry_level,
+            'manufacturing': manufacturing,
+            'telecommunications': telecommunications,
+            'executive': executive,
+            'marketing': marketing,
+            'training': training,
+            'facilities': facilities,
+            'media': media,
+            'newspaper': newspaper,
+            'transportation': transportation,
+            'finance': finance,
+            'nonprofit': nonprofit,
+            'social_services': social_services,
+            'warehouse': warehouse,
+            'other': other
+        }
 
     )
 
--- static/js/career_chart.js
+++ static/js/career_chart.js
@@ -29,48 +29,239 @@
         series: [{
             name: "My Friends' Careers",
             colorByPoint: true,
-            data: [{
-                name: "Software Development",
-                y: 0
-            }, {
-                name: "Customer Service",
-                y: 23.53,
-                sliced: true,
-                selected: true
-            }, {
-                name: "Web Services",
-                y: 11.76
-            }, {
-                name: "Other",
-                y: 5.8
-            }, {
-                name: "Engineer",
-                y: 5.8
-            }, {
-                name: "Accounting/Bookkeeping",
-                y: 5.8
-            }, {
-                name: "Healthcare",
-                y: 5.8
-            }, {
-                name: "Retired",
-                y: 11.6
-            }, {
-                name: "Teaching",
-                y: 5.8
-            }, {
-                name: "Management",
-                y: 5.8
-            }, {
-                name: "Graphic Design",
-                y: 5.8
-            }, {
-                name: "Product Developer",
-                y: 5.8
-            }, {
-                name: "Editor",
-                y: 5.8
-            }]
+            data: [
+                {
+                    name: "Accounting",
+                    y: accounting
+                },
+                {
+                    name: "general_business",
+                    y: general_business
+                },
+
+                {
+                    name: "Software Development",
+                    y: software_development,
+                    sliced: true,
+                    selected: true
+                },
+                {
+                    name: "admin_clerical",
+                    y: admin_clerical
+                },
+                {
+                    name: "general_labor",
+                    y: general_labor
+                },
+                {
+                    name: "pharmaceutical",
+                    y: pharmaceutical
+                },
+                {
+                    name: "automotive",
+                    y: automotive
+                },
+                {
+                    name: "government",
+                    y: government
+                },
+                {
+                    name: "professional_services",
+                    y: professional_services
+                },
+                {
+                    name: "banking",
+                    y: banking
+                },
+                {
+                    name: "grocery",
+                    y: grocery
+                },
+                {
+                    name: "purchasing_procurement",
+                    y: purchasing_procurement
+                },
+                {
+                    name: "biotech",
+                    y: biotech
+                },
+                {
+                    name: "health_care",
+                    y: health_care
+                },
+                {
+                    name: "qa_quality_control",
+                    y: qa_quality_control
+                },
+                {
+                    name: "journalism",
+                    y: journalism
+                },
+                {
+                    name: "hotel_hospitality",
+                    y: hotel_hospitality
+                },
+                {
+                    name: "Real_Estate",
+                    y: Real_Estate
+                },
+                {
+                    name: "business_development",
+                    y: business_development
+                },
+                {
+                    name: "human_resources",
+                    y: human_resources
+                },
+                {
+                    name: "research",
+                    y: research
+                },
+                {
+                    name: "construction",
+                    y: construction
+                },
+                {
+                    name: "information_technology",
+                    y: information_technology
+                },
+                {
+                    name: "restaurant_food_service",
+                    y: restaurant_food_service
+                },
+                {
+                    name: "consultant",
+                    y: consultant
+                },
+                {
+                    name: "installation_repair",
+                    y: installation_repair
+                },
+                {
+                    name: "retail",
+                    y: retail
+                },
+                {
+                    name: "customer_service",
+                    y: customer_service
+                },
+                {
+                    name: "insurance",
+                    y: insurance
+                },
+                {
+                    name: "sales",
+                    y: sales
+                },
+                {
+                    name: "design",
+                    y: design
+                },
+                {
+                    name: "inventory",
+                    y: inventory
+                },
+                {
+                    name: "science",
+                    y: science
+                },
+                {
+                    name: "distribution_shipping",
+                    y: distribution_shipping
+                },
+                {
+                    name: "legal",
+                    y: legal
+                },
+                {
+                    name: "skilled_labor_trades",
+                    y: skilled_labor_trades
+                },
+                {
+                    name: "education_teaching",
+                    y: education_teaching
+                },
+                {
+                    name: "legal_admin",
+                    y: legal_admin
+                },
+                {
+                    name: "strategy_planning",
+                    y: strategy_planning
+                },
+                {
+                    name: "engineering",
+                    y: engineering
+                },
+                {
+                    name: "management",
+                    y: management
+                },
+                {
+                    name: "supply_chain",
+                    y: supply_chain
+                },
+                {
+                    name: "entry_level",
+                    y: entry_level
+                },
+                {
+                    name: "manufacturing",
+                    y: manufacturing
+                },
+                {
+                    name: "telecommunications",
+                    y: telecommunications
+                },
+                {
+                    name: "executive",
+                    y: executive
+                },
+                {
+                    name: "marketing",
+                    y: marketing
+                },
+                {
+                    name: "training",
+                    y: training
+                },
+                {
+                    name: "facilities",
+                    y: facilities
+                },
+                {
+                    name: "media",
+                    y: media
+                },
+                {
+                    name: "newspaper",
+                    y: newspaper
+                },
+                {
+                    name: "transportation",
+                    y: transportation
+                },
+                {
+                    name: "finance",
+                    y: finance
+                },
+                {
+                    name: "nonprofit",
+                    y: nonprofit
+                },
+                {
+                    name: "social_services",
+                    y: social_services
+                },
+                {
+                    name: "warehouse",
+                    y: warehouse
+                },
+                {
+                    name: "other",
+                    y: other
+                }
+            ]
         }]
     });
 });
